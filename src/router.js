aa4695f214cfe1d276f430a256f0ee5a4e10761f
“Richard <richgurney1987@gmail.com>
2017-03-01 12:28:26 +0000

    now production ready

--- src/router.js
+++ src/router.js
@@ -3,19 +3,31 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
 
 const componentRoutes = {
   component: Home,
   path: '/',
-  indexRoute: { component: ArtisMain},
+  indexRoute: { component: ArtistMain},
   childRoutes: [
     {
-      path: 'artists/new'
+      path: 'artists/new',
       getComponent(location, cd) {
-        
+        System.import('./components/artists/ArtistCreate')
+          .then(module => cd(null, module.default))
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cd) {
+        System.import('./components/artists/ArtistDetail')
+          .then(module => cd(null, module.default))
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cd) {
+        System.import('./components/artists/ArtistEdit')
+          .then(module => cd(null, module.default))
       }
     }
   ]
@@ -24,14 +36,7 @@ const componentRoutes = {
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
